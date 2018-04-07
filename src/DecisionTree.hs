84b185fd2c698dc4237fa4020380ebe5fe508d5b
Will Badart <wbadart@live.com>
2018-04-07 16:35:42 -0400

    A little more code golf

--- src/DecisionTree.hs
+++ src/DecisionTree.hs
@@ -1,4 +1,5 @@
-{- DecisionTree.hs
+{--
+ - DecisionTree.hs
  -
  - Simple decision tree implementation for Haskell.
  -
@@ -6,48 +7,35 @@
  - created: FEB 2018
  -}
 
-module DecisionTree (classify, mktests) where
+module DecisionTree (classify) where
 
 import Data.Function (on)
-import Data.List (find, maximumBy, nub)
-
+import Data.List (find, maximumBy)
 import Data.Map (Map)
 import qualified Data.Map as M
-import qualified Data.Set as S
 
-import Entropy (Criterion, Test)
-
-uniq :: Ord a => [a] -> [a]
-uniq = S.toList . S.fromList
+import Entropy (Criterion, Test, uniq)
 
 labelCounts :: [[String]] -> Map String Int
 labelCounts = M.fromListWith (+) . (`zip` repeat 1) . map last
-    -- M.fromListWith (\_ a -> a + 1) $ zip (map last data_) (repeat 0)
 
 prediction :: [[String]] -> String
 prediction = fst . maximumBy (compare `on` snd) . M.toList . labelCounts
-    -- let counts = labelCounts data_
-    -- in fst $ maximumBy (compare `on` snd) $ M.toList counts
 
-mktests :: [[String]] -> Int -> [Test]
-mktests data_ idx =
-    let feature_vals = uniq $ map (!!idx) data_
-        mktest v     = (==v) . (!!idx)
-    in map mktest feature_vals
+mktests :: Int -> [[String]] -> [Test]
+mktests idx = map (\v -> (==v) . (!!idx)) . uniq . map (!!idx)
 
-
-bestFeature :: [[String]] -> Criterion -> Int
-bestFeature data_ criterion =
+bestFeature :: Criterion -> [[String]] -> Int
+bestFeature criterion data_ =
     let features = [0..length (head data_) - 2]  -- -2 to exclude label
-        metric   = criterion data_
-        branches = mktests data_
-    in maximumBy (compare `on` metric . branches) features
+        branches = (`mktests` data_)
+    in  maximumBy (compare `on` criterion data_ . branches) features
 
-classify :: [[String]] -> Criterion -> [String] -> Maybe String
-classify data_ criterion tup
-    | length (nub $ map last data_) <= 1 = Just (prediction data_)
+classify :: Criterion -> [[String]] -> [String] -> Maybe String
+classify criterion data_ tup
+    | length (uniq $ map last data_) <= 1 = Just (prediction data_)
     | otherwise =
-        let ftIdx = bestFeature data_ criterion
-        in case find ($tup) $ mktests data_ ftIdx of
-            Just p  -> classify (filter p data_) criterion tup
+        let ftIdx = bestFeature criterion data_
+        in case find ($tup) $ mktests ftIdx data_ of
+            Just p  -> classify criterion (filter p data_) tup
             Nothing -> Nothing
