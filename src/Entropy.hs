0cd0d496364397295e89e93839671073497ed2fa
Will Badart <wbadart@live.com>
2018-04-07 16:45:17 -0400

    Moved helpers to bottom of file Aliased genericLength to length'

--- src/Entropy.hs
+++ src/Entropy.hs
@@ -1,4 +1,5 @@
-{- Entropy.hs
+{--
+ - Entropy.hs
  -
  - Calculate the entropy on a dataset given a
  - specific branching scheme.
@@ -12,13 +13,49 @@ module Entropy
 , gainRatio
 , Criterion
 , Test
+, uniq
 ) where
 
-import Data.List (foldl', genericLength, nub)
+import Data.List (genericLength)
+import qualified Data.Set as S
+length' = genericLength
 
 type Test      = [String] -> Bool
 type Criterion = [[String]] -> [Test] -> Float
 
+-- ============
+-- Calculations
+-- ============
+
+entropy :: [[String]] -> Float
+entropy data_ =
+    let ct l  = countBy (hasLabel l) data_
+        probs = [ct l / length' data_ | l <- labels data_]
+    in negate $ sum [p * lg p | p <- probs]
+    -- negate . sum . (\p -> p * lg p) . (/ length' data_) . countBy (haslabel)
+
+conditionalEntropy :: [[String]] -> [[String] -> Bool] -> Float
+conditionalEntropy data_ branches =
+    let ct p    = countBy p data_
+        freqs p = [ct (\tup -> p tup && hasLabel l tup) | l <- labels data_]
+        fracs p = [if f /= 0 then (f / c) * lg (f / c) else 0
+                  | (f, c) <- zip (freqs p) (repeat $ ct p)]
+        total p = negate $ sum $ fracs p
+        ent p   = (ct p / length' data_) * total p
+    in sum $ map ent branches
+
+informationGain :: Criterion
+informationGain data_ branches =
+    entropy data_ - conditionalEntropy data_ branches
+
+splitInfo :: Criterion
+splitInfo data_ branches =
+    let bits c = (c / length' data_) * lg (c / length' data_)
+    in negate $ sum $ map (bits . (`countBy` data_)) branches
+
+gainRatio :: Criterion
+gainRatio data_ branches =
+    informationGain data_ branches / splitInfo data_ branches
 
 -- =======
 -- Helpers
@@ -28,45 +65,13 @@ hasLabel :: String -> [String] -> Bool
 hasLabel label = (==label) . last
 
 countBy :: Num i => (a -> Bool) -> [a] -> i
-countBy p = genericLength . filter p
+countBy p = length' . filter p
 
 labels :: [[String]] -> [String]
-labels = nub . map last
+labels = uniq . map last
 
 lg :: Floating i => i -> i
 lg = logBase 2
 
-
--- ============
--- Calculations
--- ============
-
-entropy :: [[String]] -> Float
-entropy data_ =
-    let ct l  = countBy (hasLabel l) data_
-        probs = [ct l / genericLength data_ | l <- labels data_]
-    in negate $ sum [p * lg p | p <- probs]
-
-conditionalEntropy :: [[String]] -> [[String] -> Bool] -> Float
-conditionalEntropy data_ branches =
-    let ct p    = countBy p data_
-        freqs p = [ct (\tup -> p tup && hasLabel l tup) | l <- labels data_]
-        -- freqs p = [ct ((&&) <$> p <*> hasLabel l) | l <- labels data_]
-        fracs p = [if f /= 0 then (f / c) * lg (f / c) else 0
-                  | (f, c) <- zip (freqs p) (repeat $ ct p)]
-        total p = negate $ sum $ fracs p
-        ent p   = (ct p / genericLength data_) * total p
-    in sum $ map ent branches
-
-informationGain :: Criterion
-informationGain data_ branches =
-    entropy data_ - conditionalEntropy data_ branches
-
-splitInfo :: Criterion
-splitInfo data_ branches =
-    let bits c = (c / genericLength data_) * lg (c / genericLength data_)
-    in negate $ sum $ map (bits . (`countBy` data_)) branches
-
-gainRatio :: Criterion
-gainRatio data_ branches =
-    informationGain data_ branches / splitInfo data_ branches
+uniq :: Ord a => [a] -> [a]
+uniq = S.toList . S.fromList
