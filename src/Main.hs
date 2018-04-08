0995ef474fed1ffcf1384b3ba5b37561924d6a74
Will Badart <wbadart@live.com>
2018-04-08 13:32:57 -0400

    Main reflects classify interface change

--- src/Main.hs
+++ src/Main.hs
@@ -23,11 +23,10 @@ main = do
     args <- getArgs
     let fname = if not $ null args
                 then head args else "weather.csv"
-
     putStrLn $ "Using training set from " ++ fname
     hSetBuffering stdout NoBuffering
-
     contents <- readFile fname
+
     let split = splitBy (==',')
         rows = map split $ lines contents
 
@@ -36,8 +35,7 @@ main = do
         line <- getLine
 
         let tup = split line
-        print $ classify rows informationGain tup
+        print $ classify informationGain rows tup
 
         loop
-
     loop
